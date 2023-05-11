d89df7d1c5ff0eaf42a54ddd5f4bdf4570b418d4
Daniel Júnior <daniel.junior.ifpb@gmail.com>
2023-05-11 20:13:57 -0300

    feat: create generic cache module

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -3,6 +3,7 @@ import { CityEntity } from './entities/city.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Cache } from 'cache-manager';
+import { CacheService } from 'src/cache/cache.service';
 
 @Injectable()
 export class CityService {
@@ -10,20 +11,16 @@ export class CityService {
     constructor(
         @InjectRepository(CityEntity)
         private readonly cityRepository: Repository<CityEntity>,
-        @Inject(CACHE_MANAGER) private cacheManager: Cache,
+        private readonly cacheService: CacheService,
     ) { }
 
     async getAllCitiesByState(stateId: number): Promise<CityEntity[]> {
-        const citiesCached: CityEntity[] = await this.cacheManager.get(`${stateId}`);
-        if (citiesCached) {
-            return citiesCached;
-        }
-        const cities = await this.cityRepository.find({
-            where: {
-                stateId,
-            },
-        });
-        await this.cacheManager.set(`${stateId}`, cities);
-        return cities;
+        return this.cacheService.getCache<CityEntity[]>(`state_${stateId}`, () =>
+            this.cityRepository.find({
+                where: {
+                    stateId,
+                },
+            })
+        );
     }
 }
