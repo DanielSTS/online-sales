88afbfd1af4a7f2b1e587784dc2ea91ffe7c8bec
Daniel Júnior <daniel.junior.ifpb@gmail.com>
2023-05-22 14:26:57 -0300

    feat: add address controller

--- src/user/dtos/createUser.dto.ts
+++ src/user/dtos/createUser.dto.ts
@@ -3,12 +3,16 @@ import { IsString } from "class-validator";
 export class CreateUserDto {
     @IsString()
     name: string;
+
     @IsString()
     email: string;
+
     @IsString()
     phone: string;
+
     @IsString()
     cpf: string;
+
     @IsString()
     password: string;
 }
