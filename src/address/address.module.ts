72fae3c306c784ee6ba17a3325c3983516c4c837
Daniel Júnior <daniel.junior.ifpb@gmail.com>
2023-09-19 22:50:47 -0300

    feat: insert validation in address

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -3,9 +3,11 @@ import { AddressController } from './address.controller';
 import { AddressService } from './address.service';
 import { AddressEntity } from './entities/address.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from '../user/user.module';
+import { CityModule } from '../city/city.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AddressEntity])],
+  imports: [TypeOrmModule.forFeature([AddressEntity]), UserModule, CityModule],
   controllers: [AddressController],
   providers: [AddressService],
 })
