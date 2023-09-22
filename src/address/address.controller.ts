a2d471748e71ce71422d2d6395d4ab74d2fa513d
Daniel Júnior <daniel.junior.ifpb@gmail.com>
2023-09-21 21:27:56 -0300

    feat: create decorator UserId

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,7 +1,6 @@
 import {
   Body,
   Controller,
-  Param,
   Post,
   UsePipes,
   ValidationPipe,
@@ -11,17 +10,19 @@ import { AddressService } from './address.service';
 import { AddressEntity } from './entities/address.entity';
 import { Roles } from '../decorators/roles.decorator';
 import { UserType } from '../user/enum/user-type.enum';
+import { UserId } from '../decorators/user-id.decorator';
 @Roles(UserType.User)
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
-  @Post('/:userId')
+  @Post()
   @UsePipes(ValidationPipe)
   async createAddress(
     @Body() createAddress: CreateAddressDto,
-    @Param('userId') userId: number,
+    @UserId() userId: number,
   ): Promise<AddressEntity> {
+    console.log(userId);
     return this.addressService.createAddress(createAddress, userId);
   }
 }
