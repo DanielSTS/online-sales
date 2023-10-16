93c2837a9781861913d1a7926b76b8003d82f7cf
Daniel Júnior <daniel.junior.ifpb@gmail.com>
2023-10-16 15:56:21 -0300

    test: create test for city and state services

---
+++ src/city/__mocks__/city.mock.ts
@@ -0,0 +1,9 @@
+import { CityEntity } from '../entities/city.entity';
+
+export const cityEntityMock: CityEntity = {
+  id: 12431,
+  name: 'namemock',
+  stateId: 111,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
