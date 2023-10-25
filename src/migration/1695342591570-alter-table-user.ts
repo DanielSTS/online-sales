8b50132400db45f8c2326f817b2dad6ac5176f2d
Daniel Júnior <daniel.junior.ifpb@gmail.com>
2023-10-25 15:25:44 -0300

    feat: create category and product entities

--- src/migration/1695342591570-alter-table-user.ts
+++ src/migration/1695342591570-alter-table-user.ts
@@ -3,8 +3,8 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class AlterTableUser1695342591570 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     queryRunner.query(`
-                alter table public.user
-                    add unique(email);
+                ALTER TABLE public.user
+                    ADD unique(email);
             `);
   }
 
