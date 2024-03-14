e62029d67bb7e5b1f33b3f99a3b6918daa9fb342
Daniel Júnior <daniel.junior.ifpb@gmail.com>
2024-03-14 17:00:04 -0300

    feat: insert product in cart

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -3,12 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CartEntity } from './entities/cart.entity';
 import { InsertCartDto } from './dtos/insert-cart.dto';
+import { CartProductService } from 'src/cart-product/cart-product.service';
 
 @Injectable()
 export class CartService {
   constructor(
     @InjectRepository(CartEntity)
     private readonly cartRepository: Repository<CartEntity>,
+    private readonly cartProductService: CartProductService,
   ) {}
 
   async verifyActiveCart(userId: number): Promise<CartEntity> {
@@ -33,13 +35,13 @@ export class CartService {
   }
 
   async insertProductInCart(
-    insertCart: InsertCartDto,
+    insertCartDto: InsertCartDto,
     userId: number,
   ): Promise<CartEntity> {
     const cart = await this.verifyActiveCart(userId).catch(async () => {
       return this.createCart(userId);
     });
-
+    await this.cartProductService.insertProductInCart(insertCartDto, cart);
     return cart;
   }
 }
