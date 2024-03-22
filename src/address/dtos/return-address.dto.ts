cfe5e8d5f81d8020921f32cc7b0bedd2c53bdf0f
Daniel Júnior <daniel.junior.ifpb@gmail.com>
2024-03-21 21:43:19 -0300

    feat: create orderProduct service

--- src/address/dtos/return-address.dto.ts
+++ src/address/dtos/return-address.dto.ts
@@ -2,11 +2,13 @@ import { AddressEntity } from '../entities/address.entity';
 import ReturnCityDTO from '../../city/dtos/return-city.dto';
 
 export class ReturnAddressDTO {
+  id: number;
   complement: string;
   numberAddress: number;
   cep: string;
   city: ReturnCityDTO;
   constructor(address: AddressEntity) {
+    this.id = address.id;
     this.complement = address.complement;
     this.numberAddress = address.numberAddress;
     this.cep = address.cep;
