1ea5989448b2bf641b197b8e2e8880019e4f05e7
Daniel Júnior <daniel.junior.ifpb@gmail.com>
2024-04-22 18:23:05 -0300

    feat: amount products in list

--- src/order/dtos/return-order.dto.ts
+++ src/order/dtos/return-order.dto.ts
@@ -14,6 +14,7 @@ export class ReturnOrderDTO {
   userId: number;
   addressId: number;
   paymentId: number;
+  amountProducts?: number;
 
   constructor(order: OrderEntity) {
     this.id = order.id;
@@ -33,5 +34,6 @@ export class ReturnOrderDTO {
           (orderProduct) => new ReturnOrderProductDTO(orderProduct),
         )
       : undefined;
+    this.amountProducts = order.amountProducts;
   }
 }
